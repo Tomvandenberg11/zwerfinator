746b6bb2a2f86289fa41c9ff84c31baa3aad54e2
Tom van den Berg <tomvandenberg11@hotmail.com>
2022-01-24 10:42:51 +0100

    bringing back map and file

---
+++ src/components/map/Map.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react"
+import ReactMapGL from "react-map-gl"
+import MapMarker from "./MapMarker"
+
+const Map = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 52.50773184657685,
+    longitude: 4.96442,
+    width: "60vw",
+    height: "60vh",
+    zoom: 6,
+  })
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={(viewport) => {
+          setViewport(viewport)
+        }}
+      >
+        <MapMarker
+          key={1}
+          lat={52.485769}
+          long={4.950773}
+          title="McDonalds"
+          subtitle="Dit is de McDonalds van Purmerend"
+        />
+        <MapMarker
+          key={2}
+          lat={52.51487350463867}
+          long={4.958380699157715}
+          title="Overwhere Zuid"
+          subtitle="Dit is de wijk Overwhere Zuid"
+        />
+      </ReactMapGL>
+    </div>
+  )
+}
+
+export default Map
